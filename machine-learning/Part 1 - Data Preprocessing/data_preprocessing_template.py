e63c02b55152e13a5d78307b4a7deb3b42f03ef5
Bryce Klinker <bklinker@pillartechnology.com>
2018-09-17 21:18:43 -0500

    Finishing preprocessing template

--- machine-learning/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ machine-learning/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -12,32 +12,10 @@ dataset = pd.read_csv('Data.csv')
 features = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# Take care of missing data
-imputer = Imputer(missing_values = 'NaN', strategy='mean', axis=0)
-imputer = imputer.fit(features[:, 1:3])
-features[:, 1:3] = imputer.transform(features[:, 1:3])
-
-labelEncoder_Features = LabelEncoder()
-features[:, 0] = labelEncoder_Features.fit_transform(features[:, 0])
-
-oneHotEncoder = OneHotEncoder(categorical_features=[0])
-features = oneHotEncoder.fit_transform(features).toarray()
-
-labelEncoder_y = LabelEncoder()
-y = labelEncoder_y.fit_transform(y)
-
 # Splitting the dataset into training and test sets.
 features_train, features_test, y_train, y_test = train_test_split(features, y, test_size=0.2, random_state=0)
 
 # Feature scaling
-standardScaler_Features = StandardScaler()
-features_train = standardScaler_Features.fit_transform(features_train)
-features_test = standardScaler_Features.transform(features_test)
-
-print(features)
-print(features_train)
-print(features_test)
-
-print(y)
-print(y_test)
-print(y_train)
+# standardScaler_Features = StandardScaler()
+# features_train = standardScaler_Features.fit_transform(features_train)
+# features_test = standardScaler_Features.transform(features_test)
