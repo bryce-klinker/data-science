eb9a5bd961fc494b2bb8cd53ccc2f3a12b38d967
Bryce Klinker <bklinker@pillartechnology.com>
2018-09-17 21:17:14 -0500

    Added scaling and splitting of data set to tempalte

--- machine-learning/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ machine-learning/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -4,7 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
@@ -25,5 +26,18 @@ features = oneHotEncoder.fit_transform(features).toarray()
 labelEncoder_y = LabelEncoder()
 y = labelEncoder_y.fit_transform(y)
 
+# Splitting the dataset into training and test sets.
+features_train, features_test, y_train, y_test = train_test_split(features, y, test_size=0.2, random_state=0)
+
+# Feature scaling
+standardScaler_Features = StandardScaler()
+features_train = standardScaler_Features.fit_transform(features_train)
+features_test = standardScaler_Features.transform(features_test)
+
 print(features)
+print(features_train)
+print(features_test)
+
 print(y)
+print(y_test)
+print(y_train)
